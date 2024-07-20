719dac5072ed9bb26f8786dc3bdcbb52277264e2
Kama <kamasimbeye89@gmail.com>
2024-07-20 21:42:13 +0200

    feat:Open Modal with form modal with button click Create function to set display modal to flex

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("JavaScript is working")
+
+  //function to display modal when button is clicked
+function displayDonateModal (){
+ let donateModal = document.getElementById("donationModal")
+ donateModal.style.display = "flex";
+}
+
+
+//add event listener to button
+document.addEventListener('DOMContentLoaded', function(){
+    let donateButton=document.getElementById('donateButton');
+    donateButton.addEventListener('click', displayDonateModal)   
+})
+
+function closeModal(){
+    let donateModal = document.getElementById('donationModal')
+    donateModal.style.display="none"
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    let closeModal =document.getElementById('closeModal')
+    closeModal.addEventListener('click', closeModal);
+})
