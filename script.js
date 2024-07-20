22e2ffca252ad0152f7bab395ca3206641e28718
Kama <kamasimbeye89@gmail.com>
2024-07-20 22:53:46 +0200

    Feat: Form validation
    handle form validation by making sure all fields
    are entered correctly.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 console.log("JavaScript is working")
 
- let donateModal =document.getElementById('donationModal')
+//global variables
+ let donateModal =document.getElementById('donationModal');
+ let donationForm = document.getElementById('donationForm');
+ let confirmationMessage = document.getElementById('confirmationMessage')
   //function to display modal when button is clicked
 function displayDonateModal (){
  donateModal.style.display = "flex";
@@ -12,21 +15,39 @@ document.addEventListener('DOMContentLoaded', function(){
     let donateButton=document.getElementById('donateButton');
     donateButton.addEventListener('click', displayDonateModal)   
 })
-
+//function to close modalS
 function closeModal(){
     donateModal.style.display= "none";
     console.log("Button has been clicked");
 }
+
 
 document.addEventListener('DOMContentLoaded', function(){
     let closeModalButton =document.getElementById('closeModal')
     closeModalButton.addEventListener('click', closeModal);
 });
 
+//event listener so that modal closes when click happens anywhere outside the modal
 window.addEventListener('click', function(event){
-
-    if(event.target == donateModal){
+if(event.target == donateModal){
         closeModal();
     };
-
 });
+
+//handle form submission
+donationForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    //grab input values
+    const donationAmount = document.getElementById('donationAmount').value;
+    const donorName = document.getElementById('donorName').value;
+    const donorEmail =document.getElementById('donorEmail').value;
+
+    //validate form
+    if (donationAmount>0 && donorName && donorEmail){
+      confirmationMessage.textContent = `Thank you ${donorName} for your generous donation`
+      donationForm.reset();
+    } else{
+        confirmationMessage.textContent=`Please fill in all content correctly`
+    }
+})
