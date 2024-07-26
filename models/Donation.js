8638dd4e68653fe28899210b1ed6c33ff5f44cc0
Kama <kamasimbeye89@gmail.com>
2024-07-26 06:35:01 +0200

    Add Donation Schema,

---
+++ models/Donation.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const donationSchema = new mongoose.Schema({
+    firstName:{type: String, required: true, unique: true },
+    lastName: {type: String, required: true, unique: true },
+    email: {type: String, required: true, unique: true },
+    phone: {type: String, required: true, unique: true },
+    donationType: {type: String, required: true, unique: true },
+    paymentMethod: {type: String, required: true, unique: true },
+    amount:{type: Number, required: true, unique: true },
+    date:{type: Date, default: Date.now}
+});
+
+const Donation = mongoose.model('Donation', donationSchema);
+module.exports = Donations;
--- models/User.js
+++ models/User.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type:String, requires: true, unique: true },
+    firstName: { type:String, required: true, unique: true },
+    lastName: { type:String, required: true, unique: true },
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, unique: true}
+    password: {type: String, required: true, unique: true},
+    date: {type: Date, default: Date.now}
 });
 
 const User = mongoose.model('User', userSchema);
+module.exports = User;
